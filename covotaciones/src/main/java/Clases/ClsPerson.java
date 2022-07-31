b41078e3465babc68365ef1a2d20fc2ce7f0c500
valeria <USER@DESKTOP-V7GO0DI>
2022-07-30 21:58:24 -0500

    carga inicial

---
+++ covotaciones/src/main/java/Clases/ClsPerson.java
@@ -0,0 +1,72 @@
+package Clases;
+
+/**
+ *
+ * @lina y valeria
+ */
+public class ClsPerson {
+    
+    // Declarar
+    private String documentNumber;
+    private String name;
+    private Long phone;
+    private String mail;
+    private String address;
+   // private String type_document;
+    
+
+    // Constructor por defecto / Enviamos parámetros
+    public ClsPerson(String documentNumber, String name, Long phone, String mail, String address) {
+        this.documentNumber = documentNumber;
+        this.name = name;
+        this.phone = phone;
+        this.mail = mail;
+        this.address = address;
+        //this.type_document = type_document;
+    }
+    
+    // Getters and Setters
+    public String getDocumentNumber() {
+        return documentNumber;
+    }
+
+    public void setDocumentNumber(String documentNumber) {
+        this.documentNumber = documentNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public Long getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Long phone) {
+        this.phone = phone;
+    }
+    
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    
+    
+    
+}
