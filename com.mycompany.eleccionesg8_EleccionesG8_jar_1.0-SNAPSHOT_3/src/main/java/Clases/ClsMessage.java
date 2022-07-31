9377868d7be91db2b2cbf0def7f383aa21922008
valeria <USER@DESKTOP-V7GO0DI>
2022-07-30 22:34:42 -0500

    carga inicial

---
+++ com.mycompany.eleccionesg8_EleccionesG8_jar_1.0-SNAPSHOT_3/src/main/java/Clases/ClsMessage.java
@@ -0,0 +1,27 @@
+package Clases;
+
+
+ 
+public class ClsMessage {
+
+    public static final String OK = "OK";
+    public static final String ERROR = "ERROR";
+    public static final String WARNING = "WARNING";
+
+    private String type;
+    private String text;
+
+    public ClsMessage(String type, String message) {
+        this.type = type;
+        this.text = message;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+}
